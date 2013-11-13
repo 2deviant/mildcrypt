75bc094d979903b903eda1c2574b9b2100873fbf
Val Tenyotkin <val@tenyotk.in>
2013-11-13 03:22:34 -0800

    Initial commit.

--- README.md
+++ README.md
@@ -1,4 +1,35 @@
-mildcrypt
-=========
+# MildCrypt
+Ever sit at a coffee shop, working on your app, and feeling the need to look
+over your shoulder every time you edit the settings/configuration file because
+the [database] passwords are just stting there in cleartext asking to be stolen?
+If you are a software developer with a modicum of security consciousness, you
+have.  MildCrypt solves that problem by providing a mild layer of encryption and
+obfuscation to any string of text in a variety of programming languages 
+JavaScript and PHP, for now).  If someone looks over your shoulder or takes a
+picture of your screen, (s)he will not be able to steal the access codes (a.k.a
+passwords).
 
-Security Through Obscurity 2.0
+## Example
+MildCrypt turns this:
+```javascript
+var password = 'monkey2sace';
+```
+into this:
+```javascript
+var password = 
+```
+
+## Use
+Go to the [MildCrypt website](http://2deviant.github.io/mildcrypt).
+
+## Use Cases
+
+### Passwords
+Situation described in the introduction.
+
+### Emails
+Want to post your email on a website **and** give people the ability to copy and
+paste it? Wasn't a problem until the advent of email-harvesting bots.  Said bots
+either can't or don't interpret (complex) JavaScript.  Inserting a snippet of
+JavaScript code instead of cleartext email beats the spam bots.
+
--- mildcrypt.js
+++ mildcrypt.js
@@ -1,1 +1,84 @@
- 
+// this application does not necessitate cryptographic quality random numbers
+// low quality, barely uniform implementation of Math.random() will do
+function random_key(length) {
+    var key = [];
+    while(length--)
+        key.push(Math.round(Math.random()*255));
+    return key;
+}
+
+// cipher = text ^ key
+function encode(text, key) {
+    var cipher = [];
+    for(var i = text.length; i-->0;)
+        cipher[i] = text.charCodeAt(i) ^ key[i % key.length];
+    return cipher;
+}
+
+// obfuscator dictionary, see http://www.jsfuck.com 
+function obfuscator() {
+
+    var one = '+!+![]';
+    var dictionary = {};
+    var digits = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+    for(var i in digits)
+        dictionary[digits[i]] = Array(digits[i]+1).join(one);
+
+    dictionary[0] = '+[]';
+
+    return dictionary;
+}
+
+// number obfuscator
+function fubar(x) {
+
+    var garbage = [];
+
+    // obtain the obfuscator dictionary
+    var dictionary = obfuscator();
+
+    // convert the number to a string
+    x = x.toString();
+
+    // obfuscate the number digit by digit
+    for(var i = 0; i < x.length; i++)
+        garbage.push('['+dictionary[x[i]]+']');
+
+    return garbage.join('+');
+
+}
+
+// self-explanatory
+function fubar_array(array) {
+    for(var i = array.length; i-->0;)
+        array[i] = fubar(array[i]);
+}
+
+
+
+// create a JavaScirpt MildCrypt
+function JavaScript(text) {
+
+    var key_length  = 16;
+    var key         = random_key(key_length);
+    var cipher      = encode(text, key);
+    var JS          = '';
+
+    // obfuscate the cipher and the key
+    fubar_array(cipher);
+    fubar_array(key);
+
+    // construct the self-decrypting pseudo string of text
+    JS  = 'var _=""; var __=['
+        + cipher.toString()
+        + '];for(var $ in __)_+=String.fromCharCode(__[$]^['
+        + key.toString()
+        + '][$%'
+        + key.length
+        + ']);';
+
+    JS = '(function() {' + JS + 'return _;})()'
+
+    return JS;
+}
